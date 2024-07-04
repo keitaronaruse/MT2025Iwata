e008f099939b9f5ca0db8f558dc330dc53d87f2a
keitaronaruse <naruse@u-aizu.ac.jp>
2024-07-04 15:40:38 +0900

    updated

--- iwata-03/iwata-03.cpp
+++ iwata-03/iwata-03.cpp
@@ -40,11 +40,11 @@ const double u_start = 1.800, v_start = 0.000, q_start = deg2rad( 270.0 );
 //  Goal position and angle
 const double u_goal = 2.400, v_goal = -0.600, q_goal = deg2rad( 0.0 );
 
-// //  Data Set 3
+// //  Data Set 2
 // //  Range of u-position [ u_min, u_max )
-// const double d_u = 0.0025, u_min = 2.400 - d_u / 2.0, u_max = 3.600 + d_u / 2.0;
+// const double d_u = 0.005, u_min = 2.400 - d_u / 2.0, u_max = 3.600 + d_u / 2.0;
 // //  Range of v-position [ v_min, v_max )
-// const double d_v = 0.0025, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
 // //  Range of angle [ q_min, q_max )
 // const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
 // //  Start position and angle
@@ -52,7 +52,7 @@ const double u_goal = 2.400, v_goal = -0.600, q_goal = deg2rad( 0.0 );
 // //  Goal position and angle
 // const double u_goal = 3.600, v_goal = 0.000, q_goal = deg2rad( 30.0 );
 
-// //  Data Set 4
+// //  Data Set 3
 // //  Range of u-position [ u_min, u_max )
 // const double d_u = 0.005, u_min = 3.600 - d_u / 2.0, u_max = 4.800 + d_u / 2.0;
 // //  Range of v-position [ v_min, v_max )
@@ -64,7 +64,65 @@ const double u_goal = 2.400, v_goal = -0.600, q_goal = deg2rad( 0.0 );
 // //  Goal position and angle
 // const double u_goal = 4.800, v_goal = 0.600, q_goal = deg2rad( 0.0 );
 
-//  5.400  0.000 0.161  0.000 -1.000 0.000
+// //  Data Set 4
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 4.800 - d_u / 2.0, u_max = 5.400 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 4.800, v_start = 0.600, q_start = deg2rad( 0.0 );
+// //  Goal position and angle
+// const double u_goal = 5.400, v_goal = 0.000, q_goal = deg2rad( 270.0 );
+
+// //  Data Set 5
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 4.800 - d_u / 2.0, u_max = 5.400 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 5.400, v_start = 0.600, q_start = deg2rad( 270.0 );
+// //  Goal position and angle
+// const double u_goal = 4.800, v_goal = -0.600, q_goal = deg2rad( 180.0 );
+
+// //  Data Set 6
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 3.600 - d_u / 2.0, u_max = 4.800 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 4.800, v_start = -0.600, q_start = deg2rad( 180.0 );
+// //  Goal position and angle
+// const double u_goal = 3.600, v_goal =  0.000, q_goal = deg2rad( 150.0 );
+
+// //  Data Set 7
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 2.400 - d_u / 2.0, u_max = 3.600 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 3.600, v_start = 0.000, q_start = deg2rad( 150.0 );
+// //  Goal position and angle
+// const double u_goal = 2.400, v_goal = 0.600, q_goal = deg2rad( 180.0 );
+
+// //  Data Set 8
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 1.800 - d_u / 2.0, u_max = 2.400 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 2.400, v_start = 0.000, q_start = deg2rad( 180.0 );
+// //  Goal position and angle
+// const double u_goal = 1.800, v_goal = 0.000, q_goal = deg2rad( 270.0 );
 
 /**
  * @fn operator<<
@@ -161,7 +219,7 @@ double q_val( int q_id ) { return ( double ) q_id * d_q + ( q_min + d_q / 2.0 );
 
 int main( ) {
     //  Constants
-    const int u_size = u_id( u_max ), v_size = v_id( v_max ), q_size = q_id( q_max );
+    const int u_size = u_id( u_max ) + 1, v_size = v_id( v_max ) + 1, q_size = q_id( q_max );
     std::cerr << u_size << " " << v_size << " " << q_size << std::endl;
 
     //  Cost table
@@ -204,20 +262,17 @@ int main( ) {
                 q_next -= 2.0 * M_PI;
             }
             int q_id_next = q_id( q_next );
-            assert( q_min <= q_next && q_next < q_max );
             assert( 0 <= q_id_next && q_id_next < q_size );
 
             double u_next = u_curr + V * dT * std::cos( ( q_next + q_curr ) / 2.0 );
             double v_next = v_curr + V * dT * std::sin( ( q_next + q_curr ) / 2.0 );
             int u_id_next = u_id( u_next ), v_id_next = v_id( v_next );
-
             //  Out of workspace
-            if( u_id_next < 0 || u_size <= u_id_next || v_id_next < 0 || v_size <= v_id_next ) {
+            if( !( 0 <= u_id_next && u_id_next < u_size && 0 <= v_id_next && v_id_next < v_size ) ) {
                 continue;
             }
-
-            assert( 0 <= u_id_next && u_id_next < 480 );
-            assert( 0 <= v_id_next && v_id_next < 960 );
+            assert( 0 <= u_id_next && u_id_next < u_size );
+            assert( 0 <= v_id_next && v_id_next < v_size );
 
             if( t_curr + 1.0 < g_cost.at( u_id_next ).at( v_id_next ).at( q_id_next ) ) {
                 pri_que.push( { t_curr + 1.0, { u_id_next, v_id_next, q_id_next } } );
@@ -228,12 +283,13 @@ int main( ) {
         }
     }
     std::cerr << num_searched << std::endl;
+    std::cerr << std::boolalpha << is_goal_arrived << std::endl;
 
     //  Retrieve a path
-    int u_id_curr = u_id( u_goal ), v_id_curr = v_id( v_goal ), q_id_curr = q_id( q_goal );
     std::vector< state > path_state;
+    int u_id_curr = u_id( u_goal ), v_id_curr = v_id( v_goal ), q_id_curr = q_id( q_goal );
     while( !( u_id_curr == u_id( u_start ) && ( v_id_curr == v_id( v_start ) ) && q_id_curr == q_id( q_start ) ) ) {
-        path_state.push_back( { u_id_curr, v_id_curr, q_id_curr } );
+        path_state.push_back( state( u_id_curr, v_id_curr, q_id_curr ) );
         auto [ u, v, q ] = prev.at( u_id_curr ).at( v_id_curr ).at( q_id_curr );
         u_id_curr = u;
         v_id_curr = v;
@@ -242,9 +298,8 @@ int main( ) {
         assert( 0 <= v_id_curr && v_id_curr < v_size );
         assert( 0 <= q_id_curr && q_id_curr < q_size );
     }
-    path_state.push_back( { u_id_curr, v_id_curr, q_id_curr } );
+    path_state.push_back( state( u_id_curr, v_id_curr, q_id_curr ) );
     std::reverse( path_state.begin( ), path_state.end( ) );
-    std::cerr << path_state << std::endl;
 
     //  Outout a path as ( u, v, du, dv )
     for( const auto& s : path_state ) {
