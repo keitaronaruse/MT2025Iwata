462d15ab48f6f16382d64c5f834aee46a1dfe75a
keitaronaruse <naruse@u-aizu.ac.jp>
2024-07-04 15:01:47 +0900

    updated

--- iwata-03/iwata-03.cpp
+++ iwata-03/iwata-03.cpp
@@ -22,49 +22,49 @@
  * @return rad
  * @details
  */
-double deg2rad( double deg ) {
-    return M_PI * deg / 180.0;
-}
+double deg2rad( double deg ) { return M_PI * deg / 180.0; }
 
 //  Constants
 //  INF for time
 const double INF = 1e6;
 
-// //  Data Set 1
-// //  Range of u-position [ u_min, u_max )
-// const double d_u = 0.0025, u_min = -1.0 - d_u / 2.0, u_max = 1.0 + d_u / 2.0;
-// //  Range of v-position [ v_min, v_max )
-// const double d_v = 0.0025, v_min = -1.0 - d_v / 2.0, v_max = 1.0 + d_v / 2.0;
-// //  Range of angle [ q_min, q_max )
-// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
-// //  Start position and angle
-// const double u_start = 0.000, v_start = 0.000, q_start = 0.000;
-// //  Goal position and angle
-// const double u_goal = 0.600, v_goal = 0.400, q_goal = 0.000;
+//  Data Set 1
+//  Range of u-position [ u_min, u_max )
+const double d_u = 0.005, u_min = 1.800 - d_u / 2.0, u_max = 2.400 + d_u / 2.0;
+//  Range of v-position [ v_min, v_max )
+const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+//  Range of angle [ q_min, q_max )
+const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+//  Start position and angle
+const double u_start = 1.800, v_start = 0.000, q_start = deg2rad( 270.0 );
+//  Goal position and angle
+const double u_goal = 2.400, v_goal = -0.600, q_goal = deg2rad( 0.0 );
 
-// //  Data Set 2
+// //  Data Set 3
 // //  Range of u-position [ u_min, u_max )
-// const double d_u = 0.0025, u_min = 1.800 - d_u / 2.0, u_max = 2.400 + d_u / 2.0;
+// const double d_u = 0.0025, u_min = 2.400 - d_u / 2.0, u_max = 3.600 + d_u / 2.0;
 // //  Range of v-position [ v_min, v_max )
 // const double d_v = 0.0025, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
 // //  Range of angle [ q_min, q_max )
 // const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
 // //  Start position and angle
-// const double u_start = 1.800, v_start = 0.000, q_start = deg2rad( 270.0 );
+// const double u_start = 2.400, v_start = -0.600, q_start = deg2rad( 0.0 );
 // //  Goal position and angle
-// const double u_goal = 2.400, v_goal = -0.600, q_goal = deg2rad( 0.0 );
+// const double u_goal = 3.600, v_goal = 0.000, q_goal = deg2rad( 30.0 );
 
-//  Data Set 3
-//  Range of u-position [ u_min, u_max )
-const double d_u = 0.0025, u_min = 2.400 - d_u / 2.0, u_max = 3.600 + d_u / 2.0;
-//  Range of v-position [ v_min, v_max )
-const double d_v = 0.0025, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
-//  Range of angle [ q_min, q_max )
-const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
-//  Start position and angle
-const double u_start = 2.400, v_start = -0.600, q_start = deg2rad( 0.0 );
-//  Goal position and angle
-const double u_goal = 3.600, v_goal = 0.000, q_goal = deg2rad( 30.0 );
+// //  Data Set 4
+// //  Range of u-position [ u_min, u_max )
+// const double d_u = 0.005, u_min = 3.600 - d_u / 2.0, u_max = 4.800 + d_u / 2.0;
+// //  Range of v-position [ v_min, v_max )
+// const double d_v = 0.005, v_min = -1.200 - d_v / 2.0, v_max = 1.200 + d_v / 2.0;
+// //  Range of angle [ q_min, q_max )
+// const double d_q = M_PI / 60.0, q_min = 0.0 - d_q / 2.0, q_max = 2.0 * M_PI - d_q / 2.0;
+// //  Start position and angle
+// const double u_start = 3.600, v_start = 0.000, q_start = deg2rad( 30.0 );
+// //  Goal position and angle
+// const double u_goal = 4.800, v_goal = 0.600, q_goal = deg2rad( 0.0 );
+
+//  5.400  0.000 0.161  0.000 -1.000 0.000
 
 /**
  * @fn operator<<
@@ -112,9 +112,7 @@ std::ostream& operator<<( std::ostream& os, const entry& e ) {
  * @return id
  * @details requires u_min and d_u
  */
-int u_id( double u ) {
-    return ( int ) std::floor( ( u - u_min ) / d_u );
-}
+int u_id( double u ) { return ( int ) std::floor( ( u - u_min ) / d_u ); }
 
 /**
  * @fn u_val
@@ -123,9 +121,7 @@ int u_id( double u ) {
  * @return position of u [m]
  * @details requires u_min and d_u
  */
-double u_val( int u_id ) {
-    return ( double ) u_id * d_u + u_min + d_u / 2.0;
-}
+double u_val( int u_id ) { return ( double ) u_id * d_u + u_min + d_u / 2.0; }
 
 /**
  * @fn v_id
@@ -134,9 +130,7 @@ double u_val( int u_id ) {
  * @return id
  * @details requires v_min and d_v
  */
-int v_id( double v ) {
-    return ( int ) std::floor( ( v - v_min ) / d_v );
-}
+int v_id( double v ) { return ( int ) std::floor( ( v - v_min ) / d_v ); }
 
 /**
  * @fn v_val
@@ -145,9 +139,7 @@ int v_id( double v ) {
  * @return position of v [m]
  * @details requires v_min and d_v
  */
-double v_val( int v_id ) {
-    return ( double ) v_id * d_v + ( v_min + d_v / 2.0 );
-}
+double v_val( int v_id ) { return ( double ) v_id * d_v + ( v_min + d_v / 2.0 ); }
 
 /**
  * @fn q_id
@@ -156,9 +148,7 @@ double v_val( int v_id ) {
  * @return id
  * @details requires q_min and d_q, and M_PI in cmath
  */
-int q_id( double q ) {
-    return ( int ) std::floor( ( q - q_min ) / d_q );
-}
+int q_id( double q ) { return ( int ) std::floor( ( q - q_min ) / d_q ); }
 
 /**
  * @fn q_val
@@ -167,9 +157,7 @@ int q_id( double q ) {
  * @return an angle of q [rad]
  * @details requires q_min and d_q
  */
-double q_val( int q_id ) {
-    return ( double ) q_id * d_q + ( q_min + d_q / 2.0 );
-}
+double q_val( int q_id ) { return ( double ) q_id * d_q + ( q_min + d_q / 2.0 ); }
 
 int main( ) {
     //  Constants
@@ -208,28 +196,33 @@ int main( ) {
 
         //  Take a rotation speed w out of W
         for( auto w : W ) {
+            //  Next state
             double q_next = q_curr + w * dT;
             if( q_next < q_min ) {
                 q_next += 2.0 * M_PI;
             } else if( q_max <= q_next ) {
                 q_next -= 2.0 * M_PI;
             }
+            int q_id_next = q_id( q_next );
             assert( q_min <= q_next && q_next < q_max );
+            assert( 0 <= q_id_next && q_id_next < q_size );
 
             double u_next = u_curr + V * dT * std::cos( ( q_next + q_curr ) / 2.0 );
             double v_next = v_curr + V * dT * std::sin( ( q_next + q_curr ) / 2.0 );
+            int u_id_next = u_id( u_next ), v_id_next = v_id( v_next );
 
             //  Out of workspace
-            if( u_next < u_min || u_max <= u_next || v_next < v_min || v_max <= v_next ) {
+            if( u_id_next < 0 || u_size <= u_id_next || v_id_next < 0 || v_size <= v_id_next ) {
                 continue;
             }
 
-            //  Next state
-            int u_id_next = u_id( u_next ), v_id_next = v_id( v_next ), q_id_next = q_id( q_next );
+            assert( 0 <= u_id_next && u_id_next < 480 );
+            assert( 0 <= v_id_next && v_id_next < 960 );
+
             if( t_curr + 1.0 < g_cost.at( u_id_next ).at( v_id_next ).at( q_id_next ) ) {
                 pri_que.push( { t_curr + 1.0, { u_id_next, v_id_next, q_id_next } } );
                 g_cost.at( u_id_next ).at( v_id_next ).at( q_id_next ) = t_curr + 1.0;
-                prev.at( u_id_next ).at( v_id_next ).at( q_id_next ) = { u_id_curr, v_id_curr, q_id_curr };
+                prev.at( u_id_next ).at( v_id_next ).at( q_id_next ) = state( u_id_curr, v_id_curr, q_id_curr );
                 num_searched++;
             }
         }
@@ -238,15 +231,21 @@ int main( ) {
 
     //  Retrieve a path
     int u_id_curr = u_id( u_goal ), v_id_curr = v_id( v_goal ), q_id_curr = q_id( q_goal );
-    std::vector< state > path_state = { state( u_id_curr, v_id_curr, q_id_curr ) };
-    while( prev.at( u_id_curr ).at( v_id_curr ).at( q_id_curr ) != state{ -1, -1, -1 } ) {
-        path_state.push_back( prev.at( u_id_curr ).at( v_id_curr ).at( q_id_curr ) );
+    std::vector< state > path_state;
+    while( !( u_id_curr == u_id( u_start ) && ( v_id_curr == v_id( v_start ) ) && q_id_curr == q_id( q_start ) ) ) {
+        path_state.push_back( { u_id_curr, v_id_curr, q_id_curr } );
         auto [ u, v, q ] = prev.at( u_id_curr ).at( v_id_curr ).at( q_id_curr );
         u_id_curr = u;
         v_id_curr = v;
         q_id_curr = q;
+        assert( 0 <= u_id_curr && u_id_curr < u_size );
+        assert( 0 <= v_id_curr && v_id_curr < v_size );
+        assert( 0 <= q_id_curr && q_id_curr < q_size );
     }
+    path_state.push_back( { u_id_curr, v_id_curr, q_id_curr } );
     std::reverse( path_state.begin( ), path_state.end( ) );
+    std::cerr << path_state << std::endl;
+
     //  Outout a path as ( u, v, du, dv )
     for( const auto& s : path_state ) {
         auto [ u_id_curr, v_id_curr, q_id_curr ] = s;
